0a8179be602ffe2b381b06e2ead06e0d4d61fb50
Diego <diegobermejo045@gmail.com>
2026-05-18 12:27:41 +0200

    Información del usuario ocultada hasta que no inicie sesion

--- client/src/app/shared/hud/hud.component.ts
+++ client/src/app/shared/hud/hud.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth';
@@ -14,7 +14,7 @@ import { Subscription } from 'rxjs';
   styleUrl: './hud.component.css'
 })
 export class HudComponent implements OnInit, OnDestroy {
-  isLoggedIn = false;
+  @Input() showUserDetails = false;
   bones = 50;
   muted = false;
   shopOpen = false;
@@ -41,7 +41,6 @@ export class HudComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.authSub = this.authService.user$.subscribe(user => {
-      this.isLoggedIn = !!user && !user.guest;
       this.bones = user?.bones ?? 0;
     });
 
